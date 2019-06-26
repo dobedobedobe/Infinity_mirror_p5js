c0ae45f6a746ad14d8bd3c8213316421fea7ea66
ambikajo <ambikajoshi@gmail.com>
2019-06-26 23:57:36 +0530

    removed some strange stuff

--- infinity_mirror_p5js/sketch.js
+++ infinity_mirror_p5js/sketch.js
@@ -10,7 +10,7 @@ function draw() {
     background(50);
     tint(255, 150);
     imageMode(CENTER)
-<<<<<<< HEAD
+
     //    for (let i = 40; i < 420; i += 100) {
     for (let i = 420; i > 40; i -= 50) {
         let x = width/2 + noise(i * frameCount*12);
@@ -19,7 +19,5 @@ function draw() {
         console.log(x);
         //rotate(x/10*PI / 3.0)
         image(video, x, y, i * 4, i * 3)
-=======
->>>>>>> b26a6cfa49ef3984e5e4053f9d686f286d017382
     }
 }
